e17264c8da37a02c1631466482bbca86a167388b
Denis Kniazhev <denis.k1985@gmail.com>
2014-09-20 19:08:11 +0300

    Added dialog to notify a user possible card reload consequences

--- src/main/java/com/denisk/bullshitbingochampion/PreferencesFragment.java
+++ src/main/java/com/denisk/bullshitbingochampion/PreferencesFragment.java
@@ -1,6 +1,8 @@
 package com.denisk.bullshitbingochampion;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.preference.Preference;
@@ -11,27 +13,52 @@ import android.preference.PreferenceFragment;
  * @since 12.08.14.
  */
 public class PreferencesFragment extends PreferenceFragment {
+
+    private Activity parent;
+    private Preference resetButton;
+
+    private Preference.OnPreferenceClickListener onPreferenceClickListener= new Preference.OnPreferenceClickListener() {
+        @Override
+        public boolean onPreferenceClick(Preference preference) {
+
+            new AlertDialog.Builder(parent)
+                    .setTitle(R.string.pref_title_reload)
+                    .setMessage(R.string.pref_dialog_reload)
+                    .setPositiveButton(R.string.ok, reloadDialogListener)
+                    .setNegativeButton(R.string.cancel, reloadDialogListener)
+                    .show();
+
+            return true;
+        }
+    };
+
+    private DialogInterface.OnClickListener reloadDialogListener = new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            switch (which) {
+                case DialogInterface.BUTTON_POSITIVE:
+                    Intent intent = new Intent();
+                    intent.putExtra(PreferencesActivity.RELOAD_DEFAULT_CARDS, true);
+
+                    parent.setResult(Activity.RESULT_OK, intent);
+                    parent.finish();
+
+                    break;
+            }
+        }
+    };
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.preferences);
 
-        final Activity parent = getActivity();
+        parent = getActivity();
 
-        Preference resetButton = findPreference("reload_default_cards");
-        resetButton.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-            @Override
-            public boolean onPreferenceClick(Preference preference) {
-                Intent intent = new Intent();
-                intent.putExtra(PreferencesActivity.RELOAD_DEFAULT_CARDS, true);
+        resetButton = findPreference("reload_default_cards");
 
-                parent.setResult(Activity.RESULT_OK, intent);
-                parent.finish();
-
-                return true;
-            }
-        });
+        resetButton.setOnPreferenceClickListener(onPreferenceClickListener);
 
     }
 }
